dd72391db76904afbc818fc1be8207e55424dd5b
jtparks <justinparks2021@gmail.com>
2019-10-28 12:48:57 -0700

    Added modal functionality for ater use

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+//     $(".selector").click(function() {
+//         $(".modal").toggle();
+//     });
+// });
+
+function modalShow() {
+    $(".selector").click(function() {
+        $(".modal").toggle();
+    });
+
+    $(".close").click(function() {
+        $(".modal").toggle();
+    });
+};
