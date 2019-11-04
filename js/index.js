7ef1a700552f4a4fba1f5b572f3196c99bf114f3
jtparks <justinparks2021@gmail.com>
2019-11-04 12:55:59 -0800

    next update

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
-// $(document).ready(function() {
-//     $(".selector").click(function() {
-//         $(".modal").toggle();
-//     });
-// });
-
-function modalShow() {
+//Function to open and close the CRUD modal
+function modalShow(type) {
     $(".selector").click(function() {
         $(".modal").toggle();
+        popModal(type);
     });
 
     $(".close").click(function() {
         $(".modal").toggle();
     });
 };
+
+function popModal(type){
+    var modal = document.getElementsByClassName("modal-content");
+    switch(type){
+        case "create":
+            break;
+        case "update":
+            break;
+        case "delete":
+            var text = document.createTextNode("Are you sure you want to delete this entry?")
+            modal.appendChild(text);
+            break;
+    }
+}
