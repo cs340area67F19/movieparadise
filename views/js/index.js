2c7bb88fa0849d1d392ad419af9d110ca77d267d
jtparks <justinparks2021@gmail.com>
2019-11-18 21:23:43 -0800

    Updated Modal to work properly

--- views/js/index.js
+++ views/js/index.js
@@ -3,56 +3,73 @@ function modalShow(type) {
     $(".modal").show();
     popModal(type);
 
-    $(".close").click(function() {
-        $(".modal").toggle();
-    });
-};
+    //$(".modal").click(function() {
+    //    $(".modalContents").remove();
+    //});
+}
+
+function modalHide(){
+    $(".modal").hide();
+    $(".modal-content").remove();
+}
 
 //Function to show the correct modal for creating
 function createMod(){
     var elems = ["Name","Year","Type","Rating"];
-    var modal = document.getElementsByClassName("modal-content");
-    var text = document.createTextNode("Enter information for a new movie")
-    $(modal).append(text);
+
+    //Create the modal to show
+    var modal = document.getElementsByClassName("modal");
+    var modalContent = document.createElement("div");
+    $(modalContent).addClass("modal-content");
+    var text = document.createTextNode("Enter new information")
+    $(modalContent).append(text);
 
     for(var i=0; i<4; i++){
         var input = document.createElement("input")
         input.setAttribute("Placeholder", elems[i])
-        $(modal).append(input)
+        $(modalContent).append(input)
     }
 
     var button = document.createElement("button");
     $(button).append(document.createTextNode("Create"));
     $(button).addClass("Button")
-    $(modal).append(button)
+    $(modalContent).append(button)
+    $(modal).append(modalContent);
 }
 
 //Function to show the correct modal for updating
 function updateMod(){
     var elems = ["Name","Year","Type","Rating"];
     var defaults = ["Casablanca","1942","Drama/Romance","9.6"];
-    var modal = document.getElementsByClassName("modal-content");
-    var text = document.createTextNode("Enter information for a new movie")
-    $(modal).append(text);
+    
+    //Create the modal to show
+    var modal = document.getElementsByClassName("modal");
+    var modalContent = document.createElement("div");
+    $(modalContent).addClass("modal-content");
+    var text = document.createTextNode("Update existing information")
+    $(modalContent).append(text);
 
     for(var i=0; i<4; i++){
         var input = document.createElement("input")
         input.setAttribute("Placeholder", elems[i])
         input.value = defaults[i]
-        $(modal).append(input)
+        $(modalContent).append(input)
     }
 
     var button = document.createElement("button");
     $(button).append(document.createTextNode("Update"));
     $(button).addClass("Button")
-    $(modal).append(button)
+    $(modalContent).append(button)
+    $(modal).append(modalContent);
 }
 
 //Function to show the correct modal for deleting
 function deleteMod(){
-    var modal = document.getElementsByClassName("modal-content");
+    var modal = document.getElementsByClassName("modal");
+    var modalContent = document.createElement("div");
+    $(modalContent).addClass("modal-content");
     var text = document.createTextNode("Are you sure you want to delete this entry?")
-    $(modal).append(text);
+    $(modalContent).append(text);
     
     //Create the delete buttons
     var button = document.createElement("button");
@@ -61,8 +78,9 @@ function deleteMod(){
     var button2 = document.createElement("button");
     $(button2).append(document.createTextNode("Cancel"));
     $(button2).addClass("Button");
-    $(modal).append(button)
-    $(modal).append(button2)
+    $(modalContent).append(button)
+    $(modalContent).append(button2)
+    $(modal).append(modalContent);
 }
 
 //Function to determine which modal to show
@@ -79,3 +97,4 @@ function popModal(type){
             break;
     }
 }
+
