229e7b4f0322ca57da60ba37f3d7a3b23020091c
HeySiri1998 <34022268+Sing1998@users.noreply.github.com>
2019-11-18 18:58:17 -0800

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -1,6 +1,18 @@
+--
+-- MovieList 
+--
+select movie.name FROM movie;
+
+
+--
+-- MovieInfo page 
+--
+select movie.name, year, movie_type, country, length FROM movie where movie_id =1;
+
+--
+-- CastList page 
+--
 select fname, lname, cast_fname, cast_lname from movie
 INNER JOIN movie_cast ON movie_cast_movie_id = movie_id
 INNER JOIN cast ON cast_id = movie_cast_cast_id 
 where movie_id = 1;
-
-select movie.name, year, movie_type, country, length FROM movie where movie_id =1;
