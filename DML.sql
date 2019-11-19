4d40c55f56edb8e5cfde0380eff178347b0a7bda
Justin Parks <justinparks2021@gmail.com>
2019-11-18 22:17:21 -0800

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -25,3 +25,10 @@ INNER JOIN review_movie_association ON review_movie_association.movie_review_id=
 INNER JOIN review ON  review_movie_association.review_id = review.review_id
 INNER JOIN users ON review.user_id = users.user_id
 where movie_id = 1
+
+--
+-- Users page
+--
+select B.*, A.*, COUNT(*) as num_reviews from users B
+inner join review A on A.user_id = B.user_id
+group by B.user_id;
